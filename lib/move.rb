5fc18e2569880cbd401dd3d4ed2dfc3012dd8657
rafeeq-123 <Rafeeq-123@hotmail.com>
2016-01-11 01:58:47 +0000

    nitrous-backup at Mon Jan 11 01:58:47 UTC 2016

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,9 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your move method here!
+def move
+  board= "array, position, value"
+  puts ["X"," "," "," "," "," "," "," ",]
+  puts= "x"
+  input= gets.strips
+end
